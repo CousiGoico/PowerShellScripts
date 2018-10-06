ef5cb80575c6ff5956e75ff06beba7dd6e72df94
Cousi <cousi@T-Mac.local>
2018-10-06 12:05:08 +0200

    Next change 3

--- get-pull.ps1
+++ get-pull.ps1
@@ -15,5 +15,20 @@ param(
     [Parameter(Mandatory)]
     [string]$scriptsRepository
 )
-Write-Host "Listo para empezar".
+
+Write-Host "*******************************";
+Write-Host "        GET PULL SCRIPT        ";
+Write-Host "*******************************";
+Write-Host "Remote repository: $remoteRepository";
+Write-Host "Project: $project";
+Write-Host "Branch: $branch";
+Write-Host "Local repository: $localRepository";
+Write-Host "Scripts repository: $scriptsRepository";
+Write-Host "*******************************";
+Write-Host "";
+Write-Host "";
+Set-Location $localRepository;
+git pull;
+git status;
+Write-Host "*******************************";
 
