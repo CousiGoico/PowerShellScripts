70c780141ddcfc496acb5398310aeea023ba73f5
Cousi <cousi@T-Mac.local>
2018-10-06 11:49:25 +0200

    Next change 2

--- get-pull.ps1
+++ get-pull.ps1
@@ -2,6 +2,23 @@ param(
     [# Remote repository to connect
     [Parameter(Mandatory)]
     [string]
-    $remoteRepository]
+    $remoteRepository],
+    [# Project of remote repository
+    [Parameter(Mandatory)]
+    [string]
+    $project],
+    [# Branch
+    [Parameter(Mandatory)]
+    [string]
+    $branch
+    [# Local repository
+    [Parameter(Mandatory)]
+    [string]
+    $localRepository],
+    [# Scripts repostory
+    [Parameter(Mandatory)]
+    [string]
+    $scriptsRepository]
 )
+Write-Host "Listo para empezar".
 
