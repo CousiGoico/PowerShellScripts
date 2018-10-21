c556c01838e48419cf667a426dd07d022783d5c8
Cousi <cousi@T-Mac.local>
2018-10-21 22:55:34 +0200

    Create a script powershell

---
+++ Create-Branch.ps1
@@ -0,0 +1,24 @@
+﻿param( 
+    [Parameter(Mandatory=$true)]
+    [string]$repository = "",
+    [Parameter(Mandatory=$true)]
+    [string]$localPathRepository = "",
+    [Parameter(Mandatory=$true)]
+    [string]$newBranchName = "",
+    [Parameter(Mandatory=$true)]
+    [string]$fromBranch = ""
+);
+
+$newBranchName = $newBranchName.ToLower();
+
+[string]$actualPath = get-location;
+set-location $localPathRepository;
+
+git checkout $fromBranch;
+git pull;
+git branch $newBranchName;
+git checkout $newBranchName;
+git push --set-upstream origin $newBranchName;
+git push;
+
+set-location $actualPath;
