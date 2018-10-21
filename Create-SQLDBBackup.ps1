a5f43ed949ccb02bc6de7a8be32978d86eb3d5e8
Cousi <cousi@T-Mac.local>
2018-10-21 22:33:35 +0200

    Create new powershell script

---
+++ Create-SQLDBBackup.ps1
@@ -0,0 +1,25 @@
+﻿param(
+    [Parameter(Mandatory)]
+    [string]$instance = "",
+    [Parameter(Mandatory)]
+    [string]$dbname = "",
+    [Parameter(Mandatory)]
+    [string]$outPath = "",
+    [string]$outFileName = ""
+);
+
+# set the out file.
+if ($outFileName -eq "") {
+    [string]$date = Get-Date -Format "yyyyMMdd";
+    $outFileName = "$outPath\$($date)_$($dbname).bak";
+}
+else {
+    [string]$extension = Split-Path $outFileName -Extension;
+    if ($extension -eq "") {
+        $outFileName = "$outFileName.bak";
+    }
+    $outFileName = "$outPath\$outFileName";
+}
+
+# Backup sql database
+Backup-SqlDatabase -ServerInstance $instance -Database $dbname -BackupFile $outFileName ;
